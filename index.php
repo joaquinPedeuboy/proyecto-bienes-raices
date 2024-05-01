aff53ed934b8d68307c206b9f76f2287de291da7
Joaquin Pedeuboy <joaquin.pedeuboy@hotmail.com>
2024-05-01 11:38:05 -0300

    Se añadio php al header

--- index.php
+++ index.php
@@ -1,41 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienes Raices</title>
-    <link rel="stylesheet" href="build/css/app.css">
-</head>
-<body>
-    
-    <header class="header inicio">
-        <div class="contenedor contenido-header">
-            <div class="barra">
-                <a href="/">
-                    <img loading="lazy" src="build/img/logo.svg" alt="logotipo de Bienes Raices">
-                </a>
-
-                <div class="mobile-menu">
-                    <img loading="lazy" src="build/img/barras.svg" alt="icono menu responsive">
-                </div>
-
-                <div class="derecha">
-                    <img class="dark-mode-boton" src="build/img/dark-mode.svg" alt="icono dark-mode">
-
-                    <nav class="navegacion">
-                        <a href="nosotros.html">Nosotros</a>
-                        <a href="anuncios.html">Anuncios</a>
-                        <a href="blog.html">Blog</a>
-                        <a href="contacto.html">Contacto</a>
-                    </nav>
-                </div>
-                
-            </div>
-
-            <h1>Venta de Casas y Departamentos Exclusivos de Lujo</h1>
-
-        </div>
-    </header>
+<?php
+    $inicio = true;
+    include './includes/templates/header.php';
+?>
 
     <main class="contenedor">
         <h1>Mas Sobre Nosotros</h1>
