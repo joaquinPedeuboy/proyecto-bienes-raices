0e81abcc725b13774cdfc173c15a618ed87f7d86
Joaquin Pedeuboy <joaquin.pedeuboy@hotmail.com>
2024-05-24 19:04:29 -0300

    Listar los vendedores y las vendedoras en la BD

--- admin/index.php
+++ admin/index.php
@@ -44,6 +44,7 @@
 
         <a href="/BienesRaices/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
 
+        <h2>Propiedades</h2>
 
         <table class="propiedades">
             <thead>
@@ -69,6 +70,36 @@
                         </form>
                         
                         <a href="/BienesRaices/admin/propiedades/actualizar.php?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
+        <h2>Vendedores</h2>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Telefono</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach( $vendedores as $vendedor ): ?>
+                <tr>
+                    <td><?php echo $vendedor->id; ?> </td>
+                    <td><?php echo $vendedor->nombre . " " . $vendedor->apellido; ?></td>
+                    <td><?php echo $vendedor->telefono; ?></td>
+                    <td>
+                        <form method="POST" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $vendedor->id; ?>">
+                            <input type="submit" class="boton-rojo-block" value="Eliminar">
+                        </form>
+                        
+                        <a href="/BienesRaices/admin/vendedores/actualizar.php?id=<?php echo $vendedor->id; ?>" class="boton-amarillo-block">Actualizar</a>
                     </td>
                 </tr>
                 <?php endforeach; ?>
